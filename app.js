84b77cb03545393e3d32e40514a6d834d3110a8c
Luis SM <luissaavedramarchant@gmail.com>
2025-02-01 12:54:39 -0300

    funcion para elegir un amigo aleatorio

--- app.js
+++ app.js
@@ -43,8 +43,25 @@ function agregarAmigo(){
           li.textContent = amigos i ;
           listaAmigos.appendChild(li);
       }
-      
+
  }
 
 
 // funcion para elegir un amigo aleatorio
+function sortearAmigos(){ 
+     //Validar que hay amigos disponibles
+     if(amigos.length == 0){ //Comprueba si el array 'amigos' esta vacio.
+        alert("No hay amigos disponibles para sortear. agregar al menos uno");
+        return;
+     }
+
+     //Genera un indice aleatorio
+     const indiceAleatorio = Math.floor(Math.random() * amigos.length); //Genera un numero aleatorio entre 0 y la longitud del array
+
+     //Obtener el nombre sorteado
+     const amigoSorteado = amigos[indiceAleatorio]; // Usa el indice aleatorio para obtener un nombre del array.
+
+     //Mostrar el resultado en el HTML
+     const resultado = document.getElementById('resultado');
+     resultado.innerHTML = 'Amigo sorteado: <strong>${amigoSorteado}<strong>';
+}
